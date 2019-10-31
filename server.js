656b7fcaff1f47e08ce5d1d88c7e5d304c13d445
Simon Peter Ojok <simonojok19@gmail.com>
2019-10-31 19:26:11 +0300

    fixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 // app.use(app.use(express.static('dist')));
 app.use(express.static(path.join(__dirname, 'dist','cinemax-theater')))
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'dist','cinemax-theater','index.html'));
 });
 
