bb0209e9387042fcbe68f954c9cebca28b98eb89
Simon Peter Ojok <simonojok19@gmail.com>
2019-10-31 19:22:02 +0300

    done with setting

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/*', (req, res) => {
+// app.use(app.use(express.static('dist')));
+app.use(express.static(path.join(__dirname, 'dist','cinemax-theater')))
+app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'dist','cinemax-theater','index.html'));
 });
+
 app.listen(port, () => {
   console.log('Server runing on port: ', port);
 });
